6bb7c797a3149f9700cae84d3c4f76095842bf6d
Zloiberg <sayraks78@gmail.com>
2022-08-12 17:15:29 +0300

    Added user token authentication and additional user API's

--- app/user/views.py
+++ app/user/views.py
@@ -1,11 +1,32 @@
 """
 Views for user API
 """
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer
-
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer,
+)
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in system"""
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new atuh token for user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage auth user"""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrive aithenticated user"""
+        return self.request.user
